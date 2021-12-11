d72223a39ee1eb382d68abd74ee010eced1b2845
Johannes Weber <johannes@drweber.de>
2021-12-10 19:16:10 -0800

    load tweets via ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 /* Return a string with elapsed time between two timestamps */
 const getElapsedTime = (ts1, ts2) => {
   const SECOND = 1000;
@@ -99,6 +74,10 @@ const renderTweets = (tweets, targetSelector) => {
   });
 };
 
+const loadTweets = (targetSelector) => {
+  console.log("LOADING TWETS");
+  $.get('/tweets', (data) => renderTweets(data, targetSelector));
+};
 
 $(document).ready(function() {
 
@@ -113,7 +92,7 @@ $(document).ready(function() {
   });
   
 
-  renderTweets(data, "#tweets");
+  loadTweets("#tweets");
 });
 
 
