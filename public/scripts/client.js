89be6db533750c0bb4bf0d6c7f0f418cd2762e4a
Johannes Weber <johannes@drweber.de>
2021-12-10 16:14:35 -0800

    create fixed navigation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,37 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const getElapsedTime = (date1, date2) => {
+
+};
+
+const createTweetElement = (tweet) => {
+  const daysAgo = tweet.created_at
+  const article = `
+    <article class="tweet">
+      <header>
+        <span class="user">
+          <img src="${tweet.user.avatars}">          
+          <span>${tweet.user.name}</span>  
+        </span>
+        <div class="user-handle">
+          ${tweet.user.handle}
+        </div>
+      </header>
+      <div>
+        ${tweet.content.text}
+      </div>
+      <footer>
+        <span class="days-ago">${daysAgo}</span>
+        <div class="action-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `;
+  return article;
+}
+
