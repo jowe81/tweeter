dfffcef6a860895605dc2a9dc701f0cd296a8ddb
Johannes Weber <johannes@drweber.de>
2021-12-10 16:28:48 -0800

    implement getElapsedTime(), renderTweets()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,13 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const getElapsedTime = (date1, date2) => {
+/* Return a string with elapsed time between two timestamps */
+const getElapsedTime = (ts1, ts2) => {
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
+  const WEEK = DAY * 7;
+  const MONTH = DAY * 30;
+  const YEAR = MONTH * 12;
 
+  //Milliseconds between timestamps
+  const ms = Math.abs(ts1 - ts2);
+
+  //Decide on a unit
+  let t;
+  if (ms < MINUTE) {
+    t = "moment";
+  } else if (ms < HOUR) {
+    t = `${Math.floor(ms / MINUTE)} minute`;
+  } else if (ms < DAY) {
+    t = `${Math.floor(ms / HOUR)} hour`;
+  } else if (ms < WEEK) {
+    t = `${Math.floor(ms / DAY)} day`;
+  } else if (ms < MONTH) {
+    t = `${Math.floor(ms / DAY)} week`;
+  } else if (ms < YEAR) {
+    t = `${Math.floor(ms / DAY)} month`;
+  }
+
+  return `${t}(s) ago`;
 };
 
 const createTweetElement = (tweet) => {
-  const daysAgo = tweet.created_at
+  const elapsedTimeStr = getElapsedTime(Date.now(), tweet.created_at);
   const article = `
     <article class="tweet">
       <header>
@@ -26,7 +78,7 @@ const createTweetElement = (tweet) => {
         ${tweet.content.text}
       </div>
       <footer>
-        <span class="days-ago">${daysAgo}</span>
+        <span class="days-ago">${elapsedTimeStr}</span>
         <div class="action-icons">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -36,5 +88,13 @@ const createTweetElement = (tweet) => {
     </article>
   `;
   return article;
-}
+};
+
+const renderTweets = (tweets, targetSelector) => {
+  tweets.forEach(tweet => {
+    $(targetSelector).append(createTweetElement(tweet));
+  });
+};
+
+renderTweets(data);
 
