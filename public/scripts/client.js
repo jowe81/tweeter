df349476e55fe358d82a97546409199bb2dc0f0b
Johannes Weber <johannes@drweber.de>
2021-12-11 18:24:29 -0800

    implement scroll-toggle-button (stretch)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -118,22 +118,21 @@ const validate = (textarea) => {
   return result;
 };
 
+//Show or hide the scroll-toggle-button
+const showScrollToggleButton = (show) => {
+  const scrollToggleButton = $("main > #scroll-toggle-button")[0];
+  //Get browser viewport w/o address & bookmark bars, scrollbars, etc (actual document viewport)
+  const html = $("html")[0];
+  const viewPortHeight = html.clientHeight;
+  const viewPortWidth = html.clientWidth;
+  //Position the button relative to the bottom right of the viewport
+  $(scrollToggleButton).css('top',`${viewPortHeight - 150}px`);
+  $(scrollToggleButton).css('left',`${viewPortWidth - 150}px`);
+  //Show/hide
+  show ? $(scrollToggleButton).fadeIn() : $(scrollToggleButton).fadeOut();
+};
+
 $(document).ready(function() {
-
-
-  //Slide the new tweet form up/down when compose button is clicked
-  $("nav .new-tweet-link").on('click', function() {
-    const newTweetSection = $("main .new-tweet")[0];
-    //Use clientHeight to determine current state (0 => form is slid up/hidden)
-    if (newTweetSection.clientHeight) {
-      $(newTweetSection).slideUp();
-    } else {
-      $(newTweetSection).slideDown(function() {
-        //Pre-focus textarea after the animation completes
-        $(this).find('form > #tweet-text').focus();
-      });
-    }
-  });
 
   //Handle attempt to submit form
   $(".new-tweet form").submit(function(e) {
@@ -156,10 +155,20 @@ $(document).ready(function() {
     });
   });
 
+
+
+  //Listen to scroll events, and show scroll-toggle-button as needed
+  $(window).on('scroll', function() {
+    const scrolledTo = $(window).scrollTop();
+    showScrollToggleButton(scrolledTo > 400);
+  });
+  
   //Populate the page with tweets
   loadTweets();
   //Hide the new tweet form by default
   $(".new-tweet").slideUp(0);
+  //Hide the scroll-toggle-button by default
+  $("main > #scroll-toggle-button").hide();
 });
 
 
